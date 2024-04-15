88e162a1467a5669d4b46e5aa6867f3f4b55bf3d
Peter-cloud-web <petergichia165@gmail.com>
2024-04-16 00:06:48 +0300

    enable find user by Id

--- src/main/kotlin/com/example/repository/UserRepository.kt
+++ src/main/kotlin/com/example/repository/UserRepository.kt
@@ -3,7 +3,9 @@ package com.example.repository
 import com.example.data.model.User
 import com.example.data.tables.UserTable
 import com.example.repository.DatabaseFactory.dbQuery
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
 
 class UserRepository {
 
@@ -17,5 +19,26 @@ class UserRepository {
             }
 
         }
+    }
+
+    suspend fun findUserById(id:Int) = dbQuery {
+        UserTable.selectAll().where{
+            UserTable.userId.eq(id)
+        }
+            .map { row ->
+                rowToUser(row)
+            }
+            .singleOrNull()
+    }
+
+    private fun rowToUser(row:ResultRow?):User?{
+        if(row == null){
+            return null
+        }
+        return User(
+            user_email = row[UserTable.userEmail],
+            user_hash_password = row[UserTable.userHashPassword],
+            user_name = row[UserTable.userName]
+        )
     }
 }
