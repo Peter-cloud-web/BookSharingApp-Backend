6a8bcb01fc27cdc9d983d4f5475deb42c0a8ae7f
Peter-cloud-web <petergichia165@gmail.com>
2024-06-11 18:04:57 +0300

    Create accepted bids repository

--- src/main/kotlin/com/example/data/tables/AcceptedBidsTable.kt
+++ src/main/kotlin/com/example/data/tables/AcceptedBidsTable.kt
@@ -3,4 +3,11 @@ package com.example.data.tables
 import org.jetbrains.exposed.sql.Table
 
 object AcceptedBidsTable : Table() {
+    val id = integer("acceptedidId")
+    val bookId = integer("bookId").references(BookTable.id)
+    val bidId = integer("bidId").references(SentBidsTable.id)
+    val title = varchar("bookTitle",512)
+    val author = varchar("bookAuthor",512)
+    val pages = integer("bookPages")
+    val summary = varchar("bookSummary",512)
 }
